bba5d632c3f62d2de007f1a4ce2b857e13fa8e36
Jack Windham <jckwind11@vt.edu>
2022-07-17 17:32:26 -0400

    removed annoying comma

--- sklearn_genetic/genetic_search.py
+++ sklearn_genetic/genetic_search.py
@@ -1073,9 +1073,12 @@ class GAFeatureSelectionCV(BaseSearchCV):
         local_estimator = clone(self.estimator)
         n_selected_features = np.sum(individual)
         
-        fit_params = {}
+        fit_params = None
         if self.zero_inflated is not None:
-            fit_params = {'classifier__classifier__Z_data': self.zero_inflated, 'regressor__regressor__personality': bool_individual },
+            fit_params = {
+                'classifier__classifier__Z_data': self.zero_inflated, 
+                'regressor__regressor__personality': bool_individual 
+                }
             local_estimator.set_params(classifier__classifier__Z_data=self.X_[:, self.zero_inflated])
 
         # Compute the cv-metrics using only the selected features
@@ -1084,7 +1087,7 @@ class GAFeatureSelectionCV(BaseSearchCV):
             self.X_[:, bool_individual] if (self.zero_inflated is None) else self.X_,
             self.y_,
             cv=self.cv,
-            fit_params=None if (self.zero_inflated is None) else fit_params,
+            fit_params=fit_params,
             scoring=self.scoring,
             n_jobs=self.n_jobs,
             pre_dispatch=self.pre_dispatch,
